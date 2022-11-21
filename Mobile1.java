88764d66bbbaf2c3c15d513ec92bfbe3a3b6b377
RekhaChetanaa-nya <118670042+RekhaChetanaa-nya@users.noreply.github.com>
2022-11-21 20:30:09 +0530

    Add files via upload
    Day 1 Assignment

---
+++ Mobile1.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Mobile1 {
+
+	public static void main(String[] args) {
+		char mobileLogo='+';
+		short noOfMobilePiece=2;
+		int modelNumber=123;
+		float mobilePrice=18023.5f;
+		boolean isDamaged=false;
+		
+		System.out.println("Logo of Mobile is - "+mobileLogo);
+		System.out.println("Number of mobile piece is - "+noOfMobilePiece);
+		System.out.println("Model NUmber is - "+modelNumber);
+		System.out.println("The price of mobile is - "+mobilePrice+ " INR");
+		System.out.println("Whether the mobile is damaged (True/False)? - "+isDamaged);
+	}
+
+}
---
+++ TwoWheeler.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class TwoWheeler {
+	
+	
+	public static void main(String[] args) {
+		
+		int noOfWheels= 2;
+		float mileage=40.5f;
+		boolean isPunctured=true;
+		char bikeNameFirstLetter='Z';
+		
+		System.out.println("No of Wheels in Twowheeler is - "+noOfWheels);
+		System.out.println("Mileage is - "+mileage );
+		System.out.println("whether the Two Wheeler is Punctured (True/False)? - "+isPunctured);
+		System.out.println("First letter of the Two Wheeler - "+bikeNameFirstLetter);
+		
+	}
+
+}
