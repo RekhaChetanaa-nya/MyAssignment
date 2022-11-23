564c27b1efded9c1aba623a1500d69185534b9c1
RekhaChetanaa-nya <118670042+RekhaChetanaa-nya@users.noreply.github.com>
2022-11-23 16:55:28 +0530

    Add files via upload
    Day3

---
+++ day3/MissingElementInAnArray.java
@@ -0,0 +1,21 @@
+package week1.day3;
+
+import java.util.Arrays;
+
+public class MissingElementInAnArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr =  {1,2,3,4,7,6,8};
+		Arrays.sort(arr);
+
+		for (int i = 0; i < arr.length; i++)
+			// for (int j = 0; j < arr.length; j++) {
+			if (i + 1 != arr[i])
+			{
+				System.out.println(i+1);
+				break;
+			}
+
+	}
+}
